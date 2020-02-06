374f888accc32e936f10e36f8ece9e29481f1963
Matt Ray <github@mattray.dev>
2020-02-06 17:30:53 +1100

    add Windows logic for later

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,57 +9,61 @@ elsif platform_family?('rhel')
   include_recipe('mattray::_rhel')
 end
 
-# must have sudo
-package %w( emacs-nox sudo )
+unless platform_family?('windows')
 
-# directory for kitchen/solo
-directory '/etc/chef/trusted_certs/' do
-  recursive true
-  mode '0755'
-end
+  # must have sudo
+  package %w( emacs-nox sudo )
 
-# self-signed cert for internal A2 testing
-cookbook_file '/etc/chef/trusted_certs/ndnd_bottlebru_sh.crt' do
-  sensitive true
-  source 'ndnd_bottlebru_sh.crt'
-  mode '0644'
-end
+  # directory for kitchen/solo
+  directory '/etc/chef/trusted_certs/' do
+    recursive true
+    mode '0755'
+  end
 
-append_if_no_line 'add ndnd to /etc/hosts' do
-  path '/etc/hosts'
-  line '10.0.0.2        ndnd ndnd.bottlebru.sh'
-  line '10.0.0.4        cubert cubert.bottlebru.sh'
-end
+  # self-signed cert for internal A2 testing
+  cookbook_file '/etc/chef/trusted_certs/ndnd_bottlebru_sh.crt' do
+    sensitive true
+    source 'ndnd_bottlebru_sh.crt'
+    mode '0644'
+  end
 
-append_if_no_line 'set the PAGER for remote TRAMP sessions' do
-  path '/etc/bash.bashrc'
-  line 'export PAGER=cat'
-end
+  append_if_no_line 'add ndnd to /etc/hosts' do
+    path '/etc/hosts'
+    line '10.0.0.2        ndnd ndnd.bottlebru.sh'
+    line '10.0.0.4        cubert cubert.bottlebru.sh'
+  end
 
-user 'mattray' do
-  comment 'Matt Ray'
-  manage_home true
-  shell '/bin/bash'
-  password '$1$nzR3m/Xd$jJ3XxhiFZwuIxJXgqmUXF1'
-end
+  append_if_no_line 'set the PAGER for remote TRAMP sessions' do
+    path '/etc/bash.bashrc'
+    line 'export PAGER=cat'
+  end
 
-directory '/home/mattray/.ssh' do
-  owner 'mattray'
-  group 'mattray'
-  mode '0700'
-end
+  user 'mattray' do
+    comment 'Matt Ray'
+    manage_home true
+    shell '/bin/bash'
+    password '$1$nzR3m/Xd$jJ3XxhiFZwuIxJXgqmUXF1'
+  end
 
-cookbook_file '/home/mattray/.ssh/authorized_keys' do
-  sensitive true
-  source 'authorized_keys'
-  mode '0600'
-  owner 'mattray'
-  group 'mattray'
-end
+  directory '/home/mattray/.ssh' do
+    owner 'mattray'
+    group 'mattray'
+    mode '0700'
+  end
 
-sudo 'mattray' do
-  user 'mattray'
-  nopasswd true
+  cookbook_file '/home/mattray/.ssh/authorized_keys' do
+    sensitive true
+    source 'authorized_keys'
+    mode '0600'
+    owner 'mattray'
+    group 'mattray'
+  end
+
+  sudo 'mattray' do
+    user 'mattray'
+    nopasswd true
+  end
+
 end
 
 timezone 'Australia/Sydney'
