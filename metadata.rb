4a9931f543ec18cf651e53f40a6bea7832a43e93
Matt Ray <github@mattray.dev>
2020-02-12 12:08:17 +1100

    new macbookair recipe and switching from ndnd to inez

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Matt Ray'
 maintainer_email 'matthewhray@gmail.com'
 license          'Apache-2.0'
 description      'Configures Matt Ray\'s assorted machines'
-version          '0.19.0'
+version          '0.20.0'
 
 chef_version '>= 15'
 
