cbe77d3271548d7604cbc9f2ddc40be5b8c1e0ed
Matt Ray <github@mattray.dev>
2020-02-18 14:39:23 +1100

    migrate nodes from Hosted to ndnd

---
+++ recipes/migrate.rb
@@ -0,0 +1,47 @@
+# one-time migration from Hosted Chef to on-premises Chef Server
+
+# validator pem for ndnd
+cookbook_file '/etc/chef/chef_managed_org-validator.pem' do
+  sensitive true
+  source 'chef_managed_org-validator.pem'
+  mode '0644'
+end
+
+execute 'mv /etc/chef/client.pem /etc/chef/client.pem.old' do
+  not_if { ::File.exist?('/etc/chef/client.pem.old') }
+end
+
+# chef_server_url "https://ndnd.bottlebru.sh/organizations/chef_managed_org"
+replace_or_add "chef_server_url" do
+  path "/etc/chef/client.rb"
+  pattern "chef_server_url*"
+  line 'chef_server_url "https://ndnd.bottlebru.sh/organizations/chef_managed_org"'
+end
+
+# validation_key "/etc/chef/chef_managed_org-validator.pem"
+replace_or_add "validation_key" do
+  path "/etc/chef/client.rb"
+  pattern "validation_key*"
+  line 'validation_key "/etc/chef/chef_managed_org-validator.pem"'
+end
+
+# delete validation_client_name "chef_managed_org"
+replace_or_add "validation_key_name" do
+  path "/etc/chef/client.rb"
+  pattern 'validation_client_name "matt-validator"'
+  line '# validation_client_name "matt-validator"'
+end
+
+# policy_group "ndnd-home"
+replace_or_add "policy_group" do
+  path "/etc/chef/client.rb"
+  pattern "policy_group*"
+  line 'policy_group "ndnd-home"'
+end
+
+# policy_name "raspbian"
+replace_or_add "policy_group" do
+  path "/etc/chef/client.rb"
+  pattern "policy_name*"
+  line 'policy_name "' + node['policy_name'] + '"'
+end
