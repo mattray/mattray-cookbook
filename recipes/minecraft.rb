749522feadb32bbac099fd0f0aed7d8c2a911a6c
Matt Ray <github@mattray.dev>
2020-12-18 12:04:02 +1100

    back up twice daily

--- recipes/minecraft.rb
+++ recipes/minecraft.rb
@@ -1,5 +1,10 @@
-cron 'minecraft backups' do
+cron 'minecraft backups 1' do
+  command 'tar czf /home/minecraft/minecraft-backup-`date +\\%Y\\%m\\%d\\%H\\%M`.tgz /home/minecraft/minecraft'
+  hour '11'
+end
+
+cron 'minecraft backups 2' do
   command 'tar czf /home/minecraft/minecraft-backup-`date +\\%Y\\%m\\%d\\%H\\%M`.tgz /home/minecraft/minecraft'
   minute '30'
-  hour '14'
+  hour '16'
 end
